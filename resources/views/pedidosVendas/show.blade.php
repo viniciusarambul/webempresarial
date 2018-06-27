7aed84481a26391e6adf229f1de215a4cf4b356a
vinicius <vinicius.vieira@hotmail.com>
2018-06-27 00:39:40 -0300

    att

--- resources/views/pedidosVendas/show.blade.php
+++ resources/views/pedidosVendas/show.blade.php
@@ -21,86 +21,86 @@
             <h5>Dados do Pedido</h5>
             <p><b>Nome: </b>{{ $pedidoVenda->nome }}</p>
             <p><b>Data: </b>{{ $pedidoVenda->data }}</p>
-            <p><b>Fornecedor: </b>{{ $pedidoVenda->cliente }}</p>
             <p><b>Situacao: </b>{{ $pedidoVenda->situacao }}</p>
-            <p><b>Produto: </b>{{ $pedidoVenda->produto }}</p>
-            <p><b>Quantidade: </b>{{ $pedidoVenda->quantidade }}</p>
 
         </div>
-        <div class="col s12 m4" style="margin-top:-2.4%;">
-          <p class="card-intro">
-              &nbsp;
-              <a class="waves-effect waves-teal blue btn-floating right" href="{{ route('pedidosVendas.pedidoTitulo.create',['pedidoVenda' => $pedidoVenda->id]) }}">
-                  <i class="mdi mdi-plus"></i>
-              </a>
-          </p>
-            <div class="card">
-                <h5>Dados do Pagamento</h5>
-                <p><b>Nome: </b>{{ $pedidoVenda->nome }}</p>
-                <p><b>Data: </b>{{ $pedidoVenda->data }}</p>
-                <p><b>Situacao: </b>{{ $pedidoVenda->situacao }}</p>
+    </div>
 
-            </div>
+
+    <div class="col s12 m4" style="margin-top:-2.4%;">
+      <p class="card-intro">
+          &nbsp;
+          <a class="waves-effect waves-teal blue btn-floating right" href="{{ route('pedidosVendas.pedidoTitulo.create',['pedidoVenda' => $pedidoVenda->id]) }}">
+              <i class="mdi mdi-plus"></i>
+          </a>
+      </p>
+        <div class="card">
+            <h5>Dados do Pagamento</h5>
+            <p><b>Nome: </b>{{ $pedidoVenda->nome }}</p>
+            <p><b>Data: </b>{{ $pedidoVenda->data }}</p>
+            <p><b>Situacao: </b>{{ $pedidoVenda->situacao }}</p>
+
         </div>
+    </div>
 
 
 
-          <form class="col s12 m4" method="post" action="{{ route('pedidosVendas.destroy',['id' => $pedidoVenda->id])}}">
-            <h5>Ações</h5>
+      <form class="col s12 m4" method="post" action="{{ route('pedidosVendas.destroy',['id' => $pedidoVenda->id])}}">
+        <h5>Ações</h5>
 
-              {{ csrf_field() }}
-               <input type="hidden" name="_method" value="DELETE">
-               <button class="btn block red">Excluir</button>
-            <a class="btn blue white-text" href="{{ route('pedidosVendas.edit', ['id' => $pedidoVenda->id ]) }}"><i class="mdi mdi-pencil"></i>Editar</a>
-      </form>
+          {{ csrf_field() }}
+           <input type="hidden" name="_method" value="DELETE">
+           <button class="btn block red">Excluir</button>
+        <a class="btn blue white-text" href="{{ route('pedidosVendas.edit', ['id' => $pedidoVenda->id ]) }}"><i class="mdi mdi-pencil"></i>Editar</a>
+  </form>
 
 
-        <div class="col s12">
-            <p class="card-intro">
-                &nbsp;
-                <a class="waves-effect waves-teal blue btn-floating right" href="{{ route('pedidosVendas.pedidoItem.create',['pedidoVenda' => $pedidoVenda->id]) }}">
-                    <i class="mdi mdi-plus"></i>
-                </a>
-            </p>
-            <div class="card">
-                @if(count($pedidoVenda->itens))
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Produto</th>
-                            <th>Fornecedor</th>
-                            <th>Quantidade</th>
-                            <th>Valor</th>
-                            <th>Total</th>
+    <div class="col s12">
+        <p class="card-intro">
+            &nbsp;
+            <a class="waves-effect waves-teal blue btn-floating right" href="{{ route('pedidosVendas.pedidoItem.create',['pedidoVenda' => $pedidoVenda->id]) }}">
+                <i class="mdi mdi-plus"></i>
+            </a>
+        </p>
+        <div class="card">
+            @if(count($pedidoVenda->itens))
+            <table>
+                <thead>
+                    <tr>
+                        <th>Produto</th>
+                        <th>Fornecedor</th>
+                        <th>Quantidade</th>
+                        <th>Valor</th>
+                        <th>Total</th>
 
-                        </tr>
-                    </thead>
+                    </tr>
+                </thead>
 
-                    <tbody>
-                        @foreach($pedidoVenda->itens as $item)
-                        <tr class="with-options">
-                            <td>{{$item->produto->nome}}</td>
-                            <td>{{$item->fornecedor->nome}}</td>
-                            <td>{{$item->quantidade}}</td>
-                            <td>{{$item->preco}}</td>
-                            <td>{{$item->total}}</td>
-                            <td class="options">
-                                <a href="{{ route('pedidosVendas.show', ['$pedidoVenda' => $pedidoVenda->id]) }}">
-                                    <i class="mdi mdi-eye"></i>
-                                </a>
-                            </td>
+                <tbody>
+                    @foreach($pedidoVenda->itens as $item)
+                    <tr class="with-options">
+                        <td>{{$item->produto->nome}}</td>
+                        <td>{{$item->fornecedor->nome}}</td>
+                        <td>{{$item->quantidade}}</td>
+                        <td>{{$item->preco}}</td>
+                        <td>{{$item->total}}</td>
+                        <td class="options">
+                            <a href="{{ route('pedidosVendas.show', ['$pedidoVenda' => $pedidoVenda->id]) }}">
+                                <i class="mdi mdi-eye"></i>
+                            </a>
+                        </td>
 
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                @else
-                <p class="alert-disable">Não há produtos.</p>
-                @endif
-            </div>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            @else
+            <p class="alert-disable">Não há produtos.</p>
+            @endif
         </div>
+    </div>
 
-      </div>
+  </div>
 
 
-    @endsection
+@endsection
