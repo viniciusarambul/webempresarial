354ca4f046963746ed9ecc653703ca005bbd41db
vinicius <vinicius.vieira@hotmail.com>
2019-05-03 12:22:19 -0300

    att

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -18,15 +18,26 @@
         </script>
 
         <script>window.csrf_token = '{{ csrf_token() }}';</script>
+
+        <!-- bootstrap -->
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+
         <!--Import materialize.css-->
-        <link type="text/css" rel="stylesheet" href="{{ asset('libs/materialize/css/materialize.min.css') }}" media="screen,projection" />
+
+        <!-- <link type="text/css" rel="stylesheet" href="{{ asset('libs/materialize/css/materialize.min.css') }}" media="screen,projection" /> -->
 
         <!--    <link rel="stylesheet" href="libs/font-awesome/css/font-awesome.min.css">-->
-        <link rel="stylesheet" href="{{ asset('libs/material-icons/css/materialdesignicons.min.css') }}">
+        <!-- <link rel="stylesheet" href="{{ asset('libs/material-icons/css/materialdesignicons.min.css') }}">
 
         <link type="text/css" rel="stylesheet" href="{{ asset('css/custom.css') }}" media="screen,projection" />
         <link type="text/css" rel="stylesheet" href="{{ asset('libs/sweetAlert/sweetalert.css') }}" media="screen,projection" />
-        <link type="text/css" rel="stylesheet" href="{{ asset('libs/morris.js/morris.css') }}" media="screen,projection" />
+        <link type="text/css" rel="stylesheet" href="{{ asset('libs/morris.js/morris.css') }}" media="screen,projection" /> -->
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
        <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
@@ -142,14 +153,18 @@
         <main>
 
             <header>
-                <div class="back waves-effect waves-light">
-                    <a onclick="goBack()">
-                        <i class="mdi mdi-arrow-left"></i>
-                    </a>
-                </div>
-                <div class="icon waves-effect waves-light">
-                    <i class="{{$icon}}"></i>
-                </div>
+
+
+              <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown button
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
                 <div class="attempt">
                      <b>{{ auth()->user()->nome }}</b>
                      <small>{{ auth()->user()->email }}</small>
@@ -174,7 +189,7 @@
         <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> -->
-        <script>
+              <script>
             $(document).ready(function () {
                 app.init();
             });
